f59ad9bd90a2e25723dbb43b5c533ccbdc04ea30
Queenjul <julianainalegwu@gmail.com>
2023-04-10 14:35:33 -0700

    julall files committed

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return on sucess 1.
+ * Return: on sucess 1.
  * On error -1 is returned and error is set appropriately
  */
 int _putchar(char c)
