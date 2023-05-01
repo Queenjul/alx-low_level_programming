ebff83b666e967d0e0c9dc71064af250e847c2d1
Queenjul <julianainalegwu@gmail.com>
2023-05-01 09:53:58 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t
+ * print_listint - prints all the elements of a listint_t list.
  * @h: head of a list
  *
  * Return: number of nodes
