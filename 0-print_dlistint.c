d0c01306abcb4f69726e47c8cb11505f28ad72f3
Queenjul <julianainalegwu@gmail.com>
2023-06-15 14:11:46 -0700

    1-dlistint_len.c

--- 0-print_dlistint.c
+++ 0-print_dlistint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- *
- * @h: The head of the dlistint_t list.
- *
- * Return: The number of nodes.
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: doubly linkedlist
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t num_nodes = 0;
 
 	while (h)
 	{
-		nodes++;
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
+		num_nodes++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num_nodes);
 }
--- 1-dlistint_len.c
+++ 1-dlistint_len.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return length of linkedlist
- * @h: doubly linkedlist
- * Return: number of elements in a linked dlistint_t list
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ *
+ * @h: The head of the list.
+ *
+ * Return: The number of elements in the linked list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t length = 0;
+	size_t nodes = 0;
 
 	while (h)
-		h = h->next, length++;
+	{
+		nodes++;
+		h = h->next;
+	}
 
-	return (length);
+	return (nodes);
 }
